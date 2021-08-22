2800d5d7f918409f97bc2c3380ac04885e4b331c
Villain Deko <oksamit45@gmail.com>
2021-08-22 11:23:38 +0000

    added crsexport and crsimport, made it better

---
+++ Mewdeko.Core/Services/Database/Models/SwitchShop.cs
@@ -0,0 +1,13 @@
+﻿namespace Mewdeko.Core.Services.Database.Models
+{
+    public class SwitchShops : DbEntity
+    {
+        public ulong GuildId { get; set; }
+        public ulong Owner { get; set; }
+        public ulong ShopName { get; set; }
+        public string Status { get; set; }
+        public string Announcement { get; set; }
+        public string InviteLink { get; set; }
+        public string ExtraOwners { get; set; }
+    }
+}
