7b76d3274a63bd4a273b0b6e5d4d10bd38e68ca0
Sylveon <oksamit45@gmail.com>
2021-11-02 22:17:58 -0400

    Added age, embed and noembed checks for purge

--- src/Mewdeko/Modules/Moderation/PruneCommands.cs
+++ src/Mewdeko/Modules/Moderation/PruneCommands.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Mewdeko._Extensions;
 using Mewdeko.Common;
 using Mewdeko.Common.Attributes;
+using Mewdeko.Common.TypeReaders.Models;
 using Mewdeko.Modules.Moderation.Services;
 
 namespace Mewdeko.Modules.Moderation
@@ -46,33 +48,51 @@ namespace Mewdeko.Modules.Moderation
             [UserPerm(ChannelPermission.ManageMessages)]
             [BotPerm(ChannelPermission.ManageMessages)]
             [Priority(1)]
-            public async Task Purge(int count, string parameter = null)
+            public async Task Purge(int count, string parameter = null, StoopidTime time = null)
             {
                 count++;
                 if (count < 1)
                     return;
                 if (count > 1000)
                     count = 1000;
-
-                if (parameter == "-s" || parameter == "--safe")
+                
+                switch (parameter)
                 {
-                    await _service.PurgeWhere((ITextChannel)ctx.Channel, count, x => !x.IsPinned)
-                        .ConfigureAwait(false);
-                    return;
+                    case "-s":
+                    case "--safe":
+                        await _service.PurgeWhere((ITextChannel)ctx.Channel, count, x => !x.IsPinned)
+                            .ConfigureAwait(false);
+                        return;
+                    case "-nb":
+                    case "--nobots":
+                        await _service.PurgeWhere((ITextChannel)ctx.Channel, count, x => !x.Author.IsBot)
+                            .ConfigureAwait(false);
+                        return;
+                    case "-ob":
+                    case "--onlybots":
+                        await _service.PurgeWhere((ITextChannel)ctx.Channel, count, x => x.Author.IsBot)
+                            .ConfigureAwait(false);
+                        break;
+                    case "-a": 
+                    case "--age":
+                        if (time is null)
+                            return;
+                        if (time.Time > twoWeeks)
+                            return;
+                        await _service.PurgeWhere((ITextChannel) ctx.Channel, count, x => (x.Timestamp - DateTimeOffset.Now).TotalSeconds <= time.Time.TotalSeconds);
+                        break;
+                    case "-he":
+                    case "--hasembed":
+                        await _service.PurgeWhere((ITextChannel) ctx.Channel, count, x => x.Embeds.Any());
+                        break;
+                    case "-ne":
+                    case "--noembed":
+                        await _service.PurgeWhere((ITextChannel) ctx.Channel, count, x => !x.Embeds.Any());
+                        break;
+                    default:
+                        await _service.PurgeWhere((ITextChannel)ctx.Channel, count, x => true).ConfigureAwait(false);
+                        break;
                 }
-
-                if (parameter == "-nb" || parameter == "--nobots")
-                {
-                    await _service.PurgeWhere((ITextChannel)ctx.Channel, count, x => !x.Author.IsBot)
-                        .ConfigureAwait(false);
-                    return;
-                }
-
-                if (parameter == "-ob" || parameter == "--onlybots")
-                    await _service.PurgeWhere((ITextChannel)ctx.Channel, count, x => x.Author.IsBot)
-                        .ConfigureAwait(false);
-                else
-                    await _service.PurgeWhere((ITextChannel)ctx.Channel, count, x => true).ConfigureAwait(false);
             }
 
             //Purge @user [x]
