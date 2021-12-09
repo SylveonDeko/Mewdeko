43be2328f93504ad1c3694a3a136b02dca2db672
YunoGasai67 <59923820+YunoGasai67@users.noreply.github.com>
2021-12-08 22:57:19 -0500

    lets savee all this for now

---
+++ src/Mewdeko/Modules/Music/Extensions/IndexedLavaTrack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Discord;
+using Victoria;
+
+namespace Mewdeko.Modules.Music.Extensions
+{
+    public class IndexedLavaTrack : LavaTrack
+
+    {
+        public int Index { get; set; }
+
+        public IndexedLavaTrack(string hash, string id, string title, string author, string url, TimeSpan position, long duration, bool canSeek, bool isStream, string source)
+            : base(hash, id, title, author, url, position, duration, canSeek, isStream, source )
+        {
+            
+        }
+    }
+}
