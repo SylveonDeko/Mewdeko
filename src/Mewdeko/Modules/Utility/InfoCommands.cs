3b92b1e5f5ef9f28556de0bf7b53e8716572a6ea
YunoGasai67 <crimsonslayer76@gmail.com>
2022-01-09 01:08:19 -0500

    fixed sinfo to provide proper guild features

--- src/Mewdeko/Modules/Utility/InfoCommands.cs
+++ src/Mewdeko/Modules/Utility/InfoCommands.cs
@@ -191,7 +191,7 @@ public partial class Utility
                     .AddField("Voice Channels", voicechn.ToString())
                     .AddField("Created On", $"{createdAt:MM/dd/yyyy HH:mm}")
                     .AddField("Roles", (guild.Roles.Count - 1).ToString())
-                    .AddField("Server Features", Format.Code(string.Join("\n", vals)));
+                    .AddField("Server Features", Format.Code(string.Join("\n", setFeatures)));
                 await msg.ModifyAsync(x =>
                 {
                     x.Embed = embed.Build();
