703c6a8c7d755682d314048f304fcd2a8010dea6
YunoGasai67 <crimsonslayer76@gmail.com>
2022-01-09 00:47:29 -0500

    update help normal and slashcommand to include afk

--- src/Mewdeko/Modules/Help/Help.cs
+++ src/Mewdeko/Modules/Help/Help.cs
@@ -110,7 +110,7 @@ public class Help : MewdekoModuleBase<HelpService>
     [Aliases]
     public async Task Modules(int page = 1)
     {
-var embed = new EmbedBuilder();
+        var embed = new EmbedBuilder();
         embed.WithAuthor(new EmbedAuthorBuilder().WithIconUrl(ctx.Client.CurrentUser.RealAvatarUrl().ToString())
             .WithName("Mewdeko Help Menu"));
         embed.WithColor(Mewdeko.Services.Mewdeko.OkColor);
@@ -125,7 +125,8 @@ var embed = new EmbedBuilder();
             "\n> <:Nekoha_sleep:866321311886344202> Suggestions" +
             "\n> <:Nekoha_Yawn:866320872003076136> Server Management" +
             "\n> <:Nekoha_angy:866321279929024582> Permissions" +
-            "\n> <:Nekoha_huh:866615758032994354> Xp",
+            "\n> <:Nekoha_huh:866615758032994354> Xp" +
+            "\n> <:Nekoha_sleep:866321311886344202> Afk",
             true);
         embed.AddField("_ _",
             "> <:Nekoha_Flushed:866321565393748008> NSFW" +
@@ -133,9 +134,9 @@ var embed = new EmbedBuilder();
             "\n> <:Nekohapoke:866613862468026368> Gambling" +
             "\n> <:nekoha_slam:866316199317864458> Searches" +
             "\n> <:Nekoha_wave:866321165538164776> Games" +
-            "\n> <:Nekohaquestion:866616825750749184> Help" +
-            "\n> <:nekoha_stare:866316293179572264> Custom Reactions"
-            + "\n> <:Nekoha_Oooo:866320687810740234> Giveaways",
+            "\n> <:Nekoha_confused:866321513120399392> Help" +
+            "\n> <:nekoha_stare:866316293179572264> Custom Reactions" + 
+            "\n> <:Nekoha_Oooo:866320687810740234> Giveaways",
             true);
         embed.AddField("<:Nekohapeek:866614585992937482> Links",
             "[Documentation](https://mewdeko.tech) | [Support Server](https://discord.gg/wB9FBMreRk) | [Invite Me](https://discord.com/oauth2/authorize?client_id=752236274261426212&scope=bot&permissions=66186303&scope=bot%20applications.commands) | [Top.gg Listing](https://top.gg/bot/752236274261426212) | [Donate!](https://ko-fi.com/mewdeko) ");
