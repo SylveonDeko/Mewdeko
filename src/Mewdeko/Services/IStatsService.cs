2cbc88b601684124e4b5f0292a25fd342879c463
YunoGasai67 <crimsonslayer76@gmail.com>
2022-01-18 22:01:28 -0500

    Version 3.83

--- src/Mewdeko/Services/IStatsService.cs
+++ src/Mewdeko/Services/IStatsService.cs
@@ -2,15 +2,10 @@
 
 public interface IStatsService : INService
 {
-    long CommandsRan { get; }
     string Heap { get; }
     string Library { get; }
-    long MessageCounter { get; }
-    double MessagesPerSecond { get; }
     long TextChannels { get; }
     long VoiceChannels { get; }
-
-    TimeSpan GetUptime();
     string GetUptimeString(string separator = ", ");
     void Initialize();
 }
