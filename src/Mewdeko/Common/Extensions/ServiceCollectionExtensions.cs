895a8688a6dc7087573065a5ddc7ba14a90fb471
YunoGasai67 <59923820+YunoGasai67@users.noreply.github.com>
2022-02-04 16:53:43 -0500

    revert redis strings

--- src/Mewdeko/Common/Extensions/ServiceCollectionExtensions.cs
+++ src/Mewdeko/Common/Extensions/ServiceCollectionExtensions.cs
@@ -11,7 +11,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddBotStringsServices(this IServiceCollection services) =>
         services
             .AddSingleton<IStringsSource, LocalFileStringsSource>()
-            .AddSingleton<IBotStringsProvider, RedisBotStringsProvider>()
+            .AddSingleton<IBotStringsProvider, LocalBotStringsProvider>()
             .AddSingleton<IBotStrings, BotStrings>();
 
     public static IServiceCollection AddConfigServices(this IServiceCollection services)
