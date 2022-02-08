11fb11014a9375418e3225171a8041afd12e3741
YunoGasai67 <59923820+YunoGasai67@users.noreply.github.com>
2022-02-08 01:30:36 -0500

    Starboard Rewrite pt.1

---
+++ src/Mewdeko/Common/TypeReaders/TypeReaderCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Mewdeko.Common.TypeReaders;
+
+public class TypeReaderCollection : TypeReader
+{
+    private readonly IEnumerable<TypeReader> _readers;
+
+    public TypeReaderCollection(IEnumerable<TypeReader> readers) { _readers = readers; }
+
+    public override async Task<TypeReaderResult> ReadAsync(
+        ICommandContext context, string input, IServiceProvider services)
+    {
+        var success = new List<TypeReaderValue>();
+        var errors = new List<TypeReaderResult>();
+
+        foreach (var reader in _readers)
+        {
+            var result = await reader.ReadAsync(context, input, services);
+            if (result.Error is not null)
+                errors.Add(result);
+            else
+                success.AddRange(result.Values);
+        }
+
+        return success.Count == 0 && errors.Count > 0
+            ? errors.First()
+            : TypeReaderResult.FromSuccess(success);
+    }
+}
