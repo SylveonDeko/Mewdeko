a254a62e3e5bbe9f7788259075f3bd7c9b889704
YunoGasai67 <59923820+Sylveon76@users.noreply.github.com>
2022-02-23 16:04:25 -0500

    Fix some embed issues

--- src/Mewdeko/Common/SmartEmbed.cs
+++ src/Mewdeko/Common/SmartEmbed.cs
@@ -8,41 +8,49 @@ public class SmartEmbed
 {
     public static bool TryParse(string input, out EmbedBuilder embed, out string plainText)
     {
+        CrEmbed crembed;
         embed = null;
         plainText = string.Empty;
         if (string.IsNullOrWhiteSpace(input) || !input.Trim().StartsWith('{')) return false;
 
-       var crembed = JsonConvert.DeserializeObject<CrEmbed>(input);
-            if (!crembed.IsValid)
-            {
-                var newEmbed = JsonConvert.DeserializeObject<NewEmbed>(input);
+        try
+        {
+            crembed = JsonConvert.DeserializeObject<CrEmbed>(input);
+        }
+        catch
+        {
+            return false;
+        }
+        if (!crembed.IsValid)
+        {
+            var newEmbed = JsonConvert.DeserializeObject<NewEmbed>(input);
 
-                if (newEmbed.Embed.Fields is {Count: > 0})
-                    foreach (var f in newEmbed.Embed.Fields)
-                    {
-                        f.Name = f.Name.TrimTo(256);
-                        f.Value = f.Value.TrimTo(1024);
-                    }
-
-                if (newEmbed is {IsValid: false}) 
-                    return false;
-
-                embed = newEmbed.IsEmbedValid == false ? null : newEmbed.ToEmbed();
-                plainText = newEmbed.Content;
-                return true;
-            }
-            if (crembed is {Fields.Length: > 0})
-                foreach (var f in crembed.Fields)
+            if (newEmbed.Embed.Fields is {Count: > 0})
+                foreach (var f in newEmbed.Embed.Fields)
                 {
                     f.Name = f.Name.TrimTo(256);
                     f.Value = f.Value.TrimTo(1024);
                 }
-            
-            if (crembed is {IsValid: false}) 
+
+            if (newEmbed is {IsValid: false}) 
                 return false;
 
-            embed = crembed.IsEmbedValid == false ? null : crembed.ToEmbed();
-            plainText = crembed.PlainText;
+            embed = newEmbed.IsEmbedValid == false ? null : newEmbed.ToEmbed();
+            plainText = newEmbed.Content;
             return true;
+        }
+        if (crembed is {Fields.Length: > 0})
+            foreach (var f in crembed.Fields)
+            {
+                f.Name = f.Name.TrimTo(256);
+                f.Value = f.Value.TrimTo(1024);
+            }
+            
+        if (crembed is {IsValid: false}) 
+            return false;
+
+        embed = crembed.IsEmbedValid == false ? null : crembed.ToEmbed();
+        plainText = crembed.PlainText;
+        return true;
     }
 }
