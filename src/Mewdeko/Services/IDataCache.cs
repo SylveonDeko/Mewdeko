3d36ff0363159fdb7c4a7a828d83df374bed710c
YunoGasai67 <59923820+Sylveon76@users.noreply.github.com>
2022-02-25 11:59:03 -0500

    Highlights Continued pt.2

--- src/Mewdeko/Services/IDataCache.cs
+++ src/Mewdeko/Services/IDataCache.cs
@@ -10,20 +10,24 @@ public interface IDataCache
     IImageCache LocalImages { get; }
     ILocalDataCache LocalData { get; }
 
-    Task CacheAfk(ulong Id, List<AFK> objectList);
-    List<AFK> GetAfkForGuild(ulong Id);
-    Task AddAfkToCache(ulong Id, List<AFK> newAfk);
+    Task CacheAfk(ulong id, List<AFK> objectList);
+    List<AFK> GetAfkForGuild(ulong id);
+    Task AddAfkToCache(ulong id, List<AFK> newAfk);
     Task CacheHighlights(ulong id, List<Highlights> highlights);
     Task CacheHighlightSettings(ulong id, List<HighlightSettings> highlightSettings);
-    void CacheSnipes(ulong Id, List<SnipeStore> objectList);
-    List<SnipeStore> GetSnipesForGuild(ulong Id);
+    Task AddHighlightToCache(ulong id, List<Highlights> newHighlight);
+    Task AddHighlightSettingToCache(ulong id, List<HighlightSettings> newHighlightSetting);
+    List<Highlights> GetHighlightsForGuild(ulong id);
+    List<HighlightSettings> GetHighlightSettingsForGuild(ulong id);
+    void CacheSnipes(ulong id, List<SnipeStore> objectList);
+    List<SnipeStore> GetSnipesForGuild(ulong id);
     Task SetGuildSettingInt(ulong guildId, string setting, int value);
     Task<int> GetGuildSettingInt(ulong guildId, string setting);
     Task SetGuildSettingString(ulong guildId, string setting, string value);
     Task<string> GetGuildSettingString(ulong guildId, string setting);
     Task SetGuildSettingBool(ulong guildId, string setting, bool value);
     Task<bool> GetGuildSettingBool(ulong guildId, string setting);
-    Task AddSnipesToCache(ulong Id, List<SnipeStore> newSnipes);
+    Task AddSnipesToCache(ulong id, List<SnipeStore> newSnipes);
     Task<(bool Success, byte[] Data)> TryGetImageDataAsync(Uri key);
     Task SetImageDataAsync(Uri key, byte[] data);
     TimeSpan? AddTimelyClaim(ulong id, int period);
