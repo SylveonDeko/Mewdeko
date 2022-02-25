f6a0070fd01f71ae2902f5908ddaecce818b637d
YunoGasai67 <59923820+Sylveon76@users.noreply.github.com>
2022-02-25 07:15:09 -0500

    Highlights Continued

--- src/Mewdeko/Modules/Highlights/Services/HighlightsService.cs
+++ src/Mewdeko/Modules/Highlights/Services/HighlightsService.cs
@@ -1,6 +1,41 @@
-﻿namespace Mewdeko.Modules.Highlights.Services;
+﻿using Discord.WebSocket;
+using Mewdeko.Database;
+using Mewdeko.Database.Extensions;
+
+namespace Mewdeko.Modules.Highlights.Services;
 
 public class HighlightsService : INService
 {
+    private readonly DiscordSocketClient _client;
+    private readonly IDataCache _cache;
+    private readonly DbService _db;
+
+    public HighlightsService(DiscordSocketClient client, IDataCache cache, DbService db)
+    {
+        _client = client;
+        _cache = cache;
+        _db = db;
+        _ = CacheToRedis();
+    }
     
+    public async Task CacheToRedis()
+    {
+        var uow = _db.GetDbContext();
+        var allHighlights = uow.Highlights.AllHighlights();
+        var allHighlightSettings = uow.HighlightSettings.AllHighlightSettings();
+        var guilds = await _client.Rest.GetGuildsAsync();
+        foreach (var i in guilds)
+        {
+            await _cache.CacheHighlights(i.Id, allHighlights.Where(x => x.GuildId == i.Id).ToList());
+            await _cache.CacheHighlightSettings(i.Id, allHighlightSettings.Where(x => x.GuildId == i.Id).ToList());
+        }
+    }
+
+    public async Task AddHighlight(ulong guildId, ulong userId, string word)
+    {
+        var toadd = new Database.Models.Highlights { UserId = userId, GuildId = guildId, Word = word };
+        var uow = _db.GetDbContext();
+        uow.Highlights.Add(toadd);
+        
+    }
 }
