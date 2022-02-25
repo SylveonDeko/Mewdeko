435023a10dcf4b5196172a396a9e584deb0ee8bf
YunoGasai67 <59923820+Sylveon76@users.noreply.github.com>
2022-02-25 07:15:09 -0500

    Highlights Continued

---
+++ src/Mewdeko.Database/Models/HighlightSettings.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mewdeko.Database.Models;
+public class HighlightSettings : DbEntity
+{
+    public ulong GuildId { get; set; }
+    public ulong UserId { get; set; }
+    public string IgnoredChannels { get; set; }
+    public string IgnoredUsers { get; set; }
+    public bool HighlightsOn { get; set; }
+}
