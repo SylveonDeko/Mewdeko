7d51d4af11cc7051cd9270b6b48b2e8042bf7ff2
YunoGasai67 <59923820+Sylveon76@users.noreply.github.com>
2022-02-25 22:39:21 -0500

    AAAAAAAAAAAAA

--- src/Mewdeko/LinuxStart.sh
+++ src/Mewdeko/LinuxStart.sh
@@ -1,21 +1,16 @@
-﻿#!/usr/bin/env bash
+#!/usr/bin/env bash
 
 echo "Starting Mewdeko Build..."
 sleep 5s
 dotnet build -c Release
 cd bin/Release/net6.0/ || echo "Seems like the build failed. Please ensure you have dotnet 6 sdk installed." && exit
-if [ -n "$(ls -A data/Mewdeko.db 2>/dev/null)" ]
-  then
-    echo "Starting Mewdeko..."
-    dotnet run Mewdeko.dll 
-  else
-    echo "Running first start script..."
-    cd data
-    wget https://cdn.discordapp.com/attachments/915770282579484693/946967102680621087/Mewdeko.db
-    cd ..
-    dotnet run Mewdeko.dll
+if [ -n "$(ls -A data/Mewdeko.db 2>/dev/null)" ]; then
+  echo "Starting Mewdeko..."
+  dotnet run Mewdeko.dll
+else
+  echo "Running first start script..."
+  cd data
+  wget https://cdn.discordapp.com/attachments/915770282579484693/946967102680621087/Mewdeko.db
+  cd ..
+  dotnet run Mewdeko.dll
 fi
-    
-    
-      
-    
