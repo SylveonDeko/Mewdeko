79039d5546feb06d645dff2f82947b2d379f44c9
YunoGasai67 <59923820+Sylveon76@users.noreply.github.com>
2022-02-25 22:27:43 -0500

    Add linux start script

---
+++ src/Mewdeko/LinuxStart.sh
@@ -0,0 +1,19 @@
+﻿echo "Starting Mewdeko Build..."
+sleep 5s
+dotnet build -c Release
+cd bin/Release/net6.0/ || echo "Seems like the build failed. Please ensure you have dotnet 6 sdk installed." && exit
+if [ -n "$(ls -A data/Mewdeko.db 2>/dev/null)" ]
+  then
+    echo "Starting Mewdeko..."
+    dotnet run Mewdeko.dll 
+  else
+    echo "Running first start script..."
+    cd data
+    wget https://cdn.discordapp.com/attachments/915770282579484693/946967102680621087/Mewdeko.db
+    cd ..
+    dotnet run Mewdeko.dll
+fi
+    
+    
+      
+    
