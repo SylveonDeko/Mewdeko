c82f30fd5c53a082dfdc5caeb3af8ad81467629b
YunoGasai67 <59923820+Sylveon76@users.noreply.github.com>
2022-02-25 23:15:32 -0500

    Commit till this shit works

--- src/Mewdeko/LinuxStart.sh
+++ src/Mewdeko/LinuxStart.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-
+# shellcheck disable=SC2113
+function fail {
+    printf '%s\n' "$1" >&2 ## Send message to stderr.
+    exit "${2-1}" ## Return a code specified by $2, or 1 by default.
+}
 echo "Starting Mewdeko Build..."
 sleep 5s
 dotnet build -c Release
-cd bin/Release/net6.0/ || echo "Seems like the build failed. Please ensure you have dotnet 6 sdk installed." && exit
+cd bin/Release/net6.0/ || fail "Looks like the build failed. Please check you have dotnet installed."
 FILE=data/Mewdeko.db
 if test -f "$FILE" 
 then
