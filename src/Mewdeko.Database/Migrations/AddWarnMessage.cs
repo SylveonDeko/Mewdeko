4d7fe8b7ff764b1232e69a52e6f181aa011d86dc
YunoGasai67 <59923820+Sylveon76@users.noreply.github.com>
2022-03-06 22:20:00 -0500

    Add some stuff for later

---
+++ src/Mewdeko.Database/Migrations/AddWarnMessage.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Linq.Expressions;
+
+namespace Mewdeko.Database.Migrations;
+
+public partial class AddWarnMessage : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+        => migrationBuilder.AddColumn<string>("WarnMessage", "GuildConfig", defaultValue: "-");
+}
