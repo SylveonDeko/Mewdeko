c9299c0cb83c360db1e1f0263b0b475f0b44afa1
Sylveon <sylveon@mewdeko.tech>
2022-03-16 20:46:02 -0400

    Fix permission checks for highlight slash commands

--- src/Mewdeko/Common/Attributes/CheckPermissions.cs
+++ src/Mewdeko/Common/Attributes/CheckPermissions.cs
@@ -13,6 +13,17 @@ public sealed class CheckPermissions : PreconditionAttribute
     public override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context,
         ICommandInfo executingCommand, IServiceProvider services)
     {
+        string commandname = executingCommand.MethodName.ToLower() switch
+        {
+            "addhighlight" when executingCommand.Module.SlashGroupName == "highlights" => "highlights",
+            "listhighlights" when executingCommand.Module.SlashGroupName == "highlights" => "highlights",
+            "deletehighlight" when executingCommand.Module.SlashGroupName == "highlights" => "highlights",
+            "matchhighlight" when executingCommand.Module.SlashGroupName == "highlights" => "highlights",
+            "toggleuser" when executingCommand.Module.SlashGroupName == "highlights" => "highlights",
+            "togglechannel" when executingCommand.Module.SlashGroupName == "highlights" => "highlights",
+            "toggleglobal" when executingCommand.Module.SlashGroupName == "highlights" => "highlights",
+            _ => executingCommand.MethodName.ToLower()
+        };
         var perms = services.GetService<PermissionService>();
         var cmhandl = services.GetService<CommandHandler>();
         var groupname = executingCommand.MethodName switch
@@ -26,7 +37,7 @@ public sealed class CheckPermissions : PreconditionAttribute
         var pc = perms!.GetCacheFor(context.Guild.Id);
         int index = 0;
         return Task.FromResult(
-            pc.Permissions != null && pc.Permissions.CheckSlashPermissions(groupname, executingCommand.MethodName.ToLower(), context.User, context.Channel, out index)
+            pc.Permissions != null && pc.Permissions.CheckSlashPermissions(groupname, commandname, context.User, context.Channel, out index)
                 ? PreconditionResult.FromSuccess()
                 : PreconditionResult.FromError(perms._strings.GetText("perm_prevent", context.Guild.Id, index + 1,
                     Format.Bold(pc.Permissions[index].GetCommand(cmhandl.GetPrefix(context.Guild), context.Guild as SocketGuild)))));
