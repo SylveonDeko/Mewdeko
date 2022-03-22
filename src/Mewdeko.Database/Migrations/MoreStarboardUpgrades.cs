0bd33ea043f2d66f598d4d25aeaa9ffa9de088d4
Sylveon <sylveon@mewdeko.tech>
2022-03-22 18:10:03 -0400

    fix for compatibility with eden

--- src/Mewdeko.Database/Migrations/MoreStarboardUpgrades.cs
+++ src/Mewdeko.Database/Migrations/MoreStarboardUpgrades.cs
@@ -6,7 +6,6 @@ public partial class MoreStarboardUpgrades : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
     {
-        migrationBuilder.AddColumn<bool>("StarboardAllowBots", "GuildConfigs", defaultValue: true);
         migrationBuilder.AddColumn<bool>("StarboardRemoveOnDelete", "GuildConfigs", defaultValue: false);
         migrationBuilder.AddColumn<bool>("StarboardRemoveOnReactionsClear", "GuildConfigs", defaultValue: false);
         migrationBuilder.AddColumn<bool>("StarboardRemoveOnBelowThreshold", "GuildConfigs", defaultValue: true);
