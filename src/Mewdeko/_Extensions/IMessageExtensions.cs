f1dccbcbb5b3ba71c284ea0ceb13e46e1dea1540
Sylveon <sylveon@mewdeko.tech>
2022-03-22 15:31:40 -0400

    this should be named MessageExtensions

--- src/Mewdeko/_Extensions/IMessageExtensions.cs
+++ src/Mewdeko/_Extensions/IMessageExtensions.cs
@@ -2,7 +2,7 @@ using Discord;
 
 namespace Mewdeko._Extensions;
 
-public static class MessageExtentions
+public static class MessageExtensions
 {
     public static string GetJumpLink(this IMessage message)
         => $"https://discord.com/channels/{(message.Channel is ITextChannel channel ? channel.GuildId : "@me")}/{message.Channel.Id}/{message.Id}";
