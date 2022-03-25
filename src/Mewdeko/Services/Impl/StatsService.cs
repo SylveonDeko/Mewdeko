1d157900006a8c48ac114b33ea0a457f9fb7aad2
Sylveon <sylveon@mewdeko.tech>
2022-03-25 01:55:43 -0400

    Version 5.04

--- src/Mewdeko/Services/Impl/StatsService.cs
+++ src/Mewdeko/Services/Impl/StatsService.cs
@@ -17,7 +17,7 @@ public class StatsService : IStatsService
     public IHttpClientFactory Factory { get; }
     public IBotCredentials Creds { get; }
     public ICoordinator Coord { get; }
-    public const string BOT_VERSION = "5.03";
+    public const string BOT_VERSION = "5.04";
     
 
     private readonly DateTime _started;
