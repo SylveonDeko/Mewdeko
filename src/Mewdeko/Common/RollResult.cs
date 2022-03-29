9cea43666b0f20fa3ad4ae2afb0e1480dfbf30f3
Cat <lumitydev@gmail.com>
2022-03-29 16:33:01 -0500

    add dnd dice roller

---
+++ src/Mewdeko/Common/RollResult.cs
@@ -0,0 +1,11 @@
+namespace Mewdeko.Common;
+
+public class RollResult
+{
+    public Dictionary<Die, List<int>>? Results { get; set; }
+    public int Total { get; set; }
+    public bool InacurateTotal { get; set; }
+    public override string ToString() => $"Total: **{Total.ToString()}**";
+
+    public RollResult() => Results = new() { };
+}
