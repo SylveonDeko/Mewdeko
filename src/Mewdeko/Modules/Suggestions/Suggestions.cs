25a3f8250bd3b1d2bc78278c7c26128bd32ec77c
Sylveon <sylveon@mewdeko.tech>
2022-04-03 15:51:09 -0400

    localize help

--- src/Mewdeko/Modules/Suggestions/Suggestions.cs
+++ src/Mewdeko/Modules/Suggestions/Suggestions.cs
@@ -7,7 +7,7 @@ using Mewdeko.Extensions;
 using Mewdeko.Modules.Suggestions.Services;
 
 namespace Mewdeko.Modules.Suggestions;
-public class Suggestions : MewdekoModuleBase<SuggestionsService>
+public partial class Suggestions : MewdekoModuleBase<SuggestionsService>
 {
 
     [MewdekoCommand, Usage, Description, Aliases, RequireContext(ContextType.Guild),
--- src/Mewdeko/Modules/Tickets/Services/TicketService.cs
+++
@@ -1,7 +0,0 @@
-﻿// using Mewdeko.Services.Database.Models;
-//
-// namespace Mewdeko.Modules.Tickets.Services;
-//
-// public class TicketService : INService
-// {
-// }
