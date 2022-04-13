1dc7435758d26e5e3912c7056c07d898abb9babf
Sylveon <sylveon@mewdeko.tech>
2022-04-13 09:43:35 -0400

    update emote count on react

--- src/Mewdeko.Database/Models/Suggestions.cs
+++ src/Mewdeko.Database/Models/Suggestions.cs
@@ -8,13 +8,15 @@ public class SuggestionsModel : DbEntity
     public string Suggestion { get; set; }
     public ulong MessageID { get; set; }
     public ulong UserID { get; set; }
-    public ulong EmoteCount1 { get; set; } = 0;
-    public ulong EmoteCount2 { get; set; } = 0;
-    public ulong EmoteCount3 { get; set; } = 0;
-    public ulong EmoteCount4 { get; set; } = 0;
-    public ulong EmoteCount5 { get; set; } = 0;
+    public int EmoteCount1 { get; set; } = 0;
+    public int EmoteCount2 { get; set; } = 0;
+    public int EmoteCount3 { get; set; } = 0;
+    public int EmoteCount4 { get; set; } = 0;
+    public int EmoteCount5 { get; set; } = 0;
     public ulong StateChangeUser { get; set; } = 0;
     public ulong StateChangeCount { get; set; } = 0;
+    public ulong StateChangeMessageId { get; set; } = 0;
+    public int CurrentState = 0;
 
 
 
