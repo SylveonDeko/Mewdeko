6723fd7b0766cf22e3f0b6df06c4d475d421916d
Sylveon <sylveon@mewdeko.tech>
2022-04-13 09:43:35 -0400

    update emote count on react

---
+++ src/Mewdeko.Database/Migrations/SuggestStateMessageIds.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mewdeko.Database.Migrations;
+
+public partial class SuggestStateIds : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<ulong>("StateChangeMessageId", "Suggestions", defaultValue: 0);
+        migrationBuilder.AddColumn<int>("CurrentState", "Suggestions", defaultValue: 0);
+    }
+}
