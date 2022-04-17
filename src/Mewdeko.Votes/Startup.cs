a5269fd4d463f8617561ecafcedbc15ca3778692
Sylveon <sylveon@mewdeko.tech>
2022-04-17 10:48:35 -0400

    test

--- src/Mewdeko.Votes/Startup.cs
+++ src/Mewdeko.Votes/Startup.cs
@@ -54,8 +54,6 @@ public class Startup
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mewdeko.Votes v1"));
         }
 
-        app.UseHttpsRedirection();
-
         app.UseRouting();
 
         app.UseAuthentication();
