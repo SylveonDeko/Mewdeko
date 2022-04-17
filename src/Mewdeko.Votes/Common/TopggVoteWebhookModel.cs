27ef611461b5c6112c2b84424162402f8f2ac459
Sylveon <sylveon@mewdeko.tech>
2022-04-17 10:30:39 -0400

    maybe?

--- src/Mewdeko.Votes/Common/TopggVoteWebhookModel.cs
+++ src/Mewdeko.Votes/Common/TopggVoteWebhookModel.cs
@@ -16,11 +16,11 @@ public class TopggVoteWebhookModel
     /// The type of the vote (should always be "upvote" except when using the test button it's "test").
     /// </summary>
     public string Type { get; set; }
-        
+
     /// <summary>
     /// Whether the weekend multiplier is in effect, meaning users votes count as two.
     /// </summary>
-    public bool Weekend { get; set; }
+    public bool Weekend { get; set; } = false;
         
     /// <summary>
     /// Query string params found on the /bot/:ID/vote page. Example: ?a=1&b=2.
