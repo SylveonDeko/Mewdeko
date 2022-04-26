eb4b6e54c13d55df3dbaacd32af634e2cfc2ab68
cat <lumitydev@gmail.com>
2022-04-26 15:28:55 -0500

    add slash commands for reminders.

---
+++ src/Mewdeko/Common/Modals/ReminderModal.cs
@@ -0,0 +1,13 @@
+using Discord;
+using Discord.Interactions;
+
+namespace Mewdeko.Common.Modals;
+
+public class ReminderModal : IModal
+{
+    public string Title => "New Reminder";
+    
+    [InputLabel("reminder")]
+    [ModalTextInput("reminder", TextInputStyle.Paragraph, "Enter your reminder.")]
+    public string Reminder { get; set; }
+}
