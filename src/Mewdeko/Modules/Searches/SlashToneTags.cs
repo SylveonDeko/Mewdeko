b0dd0d39c09efa6cf7340d3e74cca34e13eb0e90
cat <lumitydev@gmail.com>
2022-04-30 00:24:45 -0500

    add interactions for tone tags

---
+++ src/Mewdeko/Modules/Searches/SlashToneTags.cs
@@ -0,0 +1,20 @@
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Mewdeko.Common;
+using Mewdeko.Common.Attributes;
+using Mewdeko.Modules.Searches.Services;
+
+namespace Mewdeko.Modules.Searches;
+
+public class SlashToneTags : MewdekoSlashModuleBase<ToneTagService>
+{
+
+    [SlashCommand("tone-tags", "Search for a specified tone tag."), BlacklistCheck, CheckPermissions]
+    public async Task Search(
+        [Summary("query", "the tone tag to search for.")] [Autocomplete(typeof(ToneTagAutocompleter))] string query) =>
+        await RespondAsync(embed: Service.GetEmbed(Service.ParseTags(query)).Build(), ephemeral: true);
+
+    [MessageCommand("Tone Tags")]
+    public async Task ToneTags(SocketMessage message) => await Search(message.Content);
+}
