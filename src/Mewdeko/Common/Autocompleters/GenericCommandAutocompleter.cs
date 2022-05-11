88fbc8dde2fd2ac424d422f20f0925a0dda31d79
Sylveon <sylveon@mewdeko.tech>
2022-05-11 00:25:40 -0400

    fix autocomplete

--- src/Mewdeko/Common/Autocompleters/GenericCommandAutocompleter.cs
+++ src/Mewdeko/Common/Autocompleters/GenericCommandAutocompleter.cs
@@ -22,21 +22,10 @@ public class GenericCommandAutocompleter : AutocompleteHandler
         _commandHandler = commandHandler;
     }
 
-    public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
-    {
-        try
-        {
-            var autoresult = _commands.Commands.Where(c => !_perms.BlockedCommands.Contains(c.Aliases[0].ToLowerInvariant()))
-                                      .Select(x => $"{x.Name} : {x.RealSummary(_strings, context.Guild?.Id, _commandHandler.GetPrefix(context.Guild?.Id))}")
-                                      .Where(x => x.Contains((string)autocompleteInteraction.Data.Current.Value, StringComparison.OrdinalIgnoreCase))
-                                      .OrderByDescending(x => x.StartsWith((string)autocompleteInteraction.Data.Current.Value, StringComparison.OrdinalIgnoreCase)).Distinct()
-                                      .Take(20).Select(x => new AutocompleteResult(x.Length >= 100 ? x[..97] + "..." : x, x.Split(':')[0].Trim()));
-            return Task.FromResult(AutocompletionResult.FromSuccess(autoresult));
-        }
-        catch (Exception e)
-        {
-            Log.Error(e.ToString());
-            return Task.FromResult(new AutocompletionResult());
-        }
-    }
+    public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services) =>
+        Task.FromResult(AutocompletionResult.FromSuccess(_commands.Commands.Where(c => !_perms.BlockedCommands.Contains(c.Aliases[0].ToLowerInvariant()))
+                                                                  .Select(x => $"{x.Name} : {x.RealSummary(_strings, context.Guild?.Id, _commandHandler.GetPrefix(context.Guild?.Id))}")
+                                                                  .Where(x => x.Contains((string)autocompleteInteraction.Data.Current.Value, StringComparison.OrdinalIgnoreCase))
+                                                                  .OrderByDescending(x => x.StartsWith((string)autocompleteInteraction.Data.Current.Value, StringComparison.OrdinalIgnoreCase)).Distinct()
+                                                                  .Take(20).Select(x => new AutocompleteResult(x.Length >= 100 ? x[..97] + "..." : x, x.Split(':')[0].Trim()))));
 }
