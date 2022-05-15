9bcfb236bf773bd6c7d2f57e36f7654251974d11
Sylveon <sylveon@mewdeko.tech>
2022-05-14 22:44:55 -0400

    fix for afkservice throwing when a message is edited

--- src/Mewdeko/Common/Autocompleters/GenericCommandAutocompleter.cs
+++ src/Mewdeko/Common/Autocompleters/GenericCommandAutocompleter.cs
@@ -10,21 +10,21 @@ namespace Mewdeko.Common.Autocompleters;
 
 public class GenericCommandAutocompleter : AutocompleteHandler
 {
-    private CommandService _commands { get; set; }
-    private CommandHandler _commandHandler { get; set; }
-    private GlobalPermissionService _perms { get; set; }
-    private IBotStrings _strings { get; set; }
+    private CommandService Commands { get; }
+    private CommandHandler CommandHandler { get; }
+    private GlobalPermissionService Perms { get; }
+    private IBotStrings Strings { get; }
     public GenericCommandAutocompleter(CommandService commands, GlobalPermissionService perms, IBotStrings strings, CommandHandler commandHandler)
     {
-        _commands = commands;
-        _perms = perms;
-        _strings = strings;
-        _commandHandler = commandHandler;
+        Commands = commands;
+        Perms = perms;
+        Strings = strings;
+        CommandHandler = commandHandler;
     }
 
     public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services) =>
-        Task.FromResult(AutocompletionResult.FromSuccess(_commands.Commands.Where(c => !_perms.BlockedCommands.Contains(c.Aliases[0].ToLowerInvariant()))
-                                                                  .Select(x => $"{x.Name} : {x.RealSummary(_strings, context.Guild?.Id, _commandHandler.GetPrefix(context.Guild?.Id))}")
+        Task.FromResult(AutocompletionResult.FromSuccess(Commands.Commands.Where(c => !Perms.BlockedCommands.Contains(c.Aliases[0].ToLowerInvariant()))
+                                                                  .Select(x => $"{x.Name} : {x.RealSummary(Strings, context.Guild?.Id, CommandHandler.GetPrefix(context.Guild?.Id))}")
                                                                   .Where(x => x.Contains((string)autocompleteInteraction.Data.Current.Value, StringComparison.OrdinalIgnoreCase))
                                                                   .OrderByDescending(x => x.StartsWith((string)autocompleteInteraction.Data.Current.Value, StringComparison.OrdinalIgnoreCase)).Distinct()
                                                                   .Take(20).Select(x => new AutocompleteResult(x.Length >= 100 ? x[..97] + "..." : x, x.Split(':')[0].Trim()))));
