a1b8476119ee243cc704ff553167eeade1a8b16f
Sylveon <sylveon@mewdeko.tech>
2022-05-15 10:13:57 -0400

    screw you rider

---
+++ src/Mewdeko/Common/Modals/CommandModal.cs
@@ -0,0 +1,13 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace Mewdeko.Common.Modals;
+
+public class CommandModal : IModal
+{
+    public string Title => "Command Input";
+    
+    [InputLabel("Args")]
+    [ModalTextInput("args", TextInputStyle.Paragraph, "Please enter command arguments"), RequiredInput(false)]
+    public string Args { get; set; }
+}
