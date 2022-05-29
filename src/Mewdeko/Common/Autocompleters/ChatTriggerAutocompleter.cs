75ab73ac0d9e4a92b995df818070a12de56531ea
cat <lumitydev@gmail.com>
2022-05-29 16:34:34 -0500

    fix chat triggers autocompleter in DMs

--- src/Mewdeko/Common/Autocompleters/ChatTriggerAutocompleter.cs
+++ src/Mewdeko/Common/Autocompleters/ChatTriggerAutocompleter.cs
@@ -8,21 +8,29 @@ namespace Mewdeko.Common.Autocompleters;
 public class ChatTriggerAutocompleter : AutocompleteHandler
 {
     public ChatTriggersService _triggers { get; set; }
+    private IBotCredentials _credentials { get; set; }
 
-    public ChatTriggerAutocompleter(ChatTriggersService triggers)
+    public ChatTriggerAutocompleter(ChatTriggersService triggers, IBotCredentials credentials)
     {
         _triggers = triggers;
+        _credentials = credentials;
     }
 
     public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context,
         IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
     {
-        var inpt = autocompleteInteraction.Data.Current.Value as string;
-        if (autocompleteInteraction.User is not IGuildUser user) return Task.FromResult(AutocompletionResult.FromError(InteractionCommandError.Unsuccessful, "You must be in a guild to use this autocompelter!"));
 
-        if (user.Guild.OwnerId != user.Id && !user.GuildPermissions.Has(GuildPermission.Administrator))
-            return Task.FromResult(AutocompletionResult.FromError(InteractionCommandError.Unsuccessful, "You must be an administrator to view chat triggers!"));
-        return Task.FromResult(AutocompletionResult.FromSuccess(_triggers.GetChatTriggersFor(user.GuildId)
+        if ((autocompleteInteraction.User is IGuildUser user
+             && user.Guild.OwnerId != user.Id
+             && !user.GuildPermissions.Has(GuildPermission.Administrator))
+            || (autocompleteInteraction.User is not IGuildUser && !_credentials.IsOwner(autocompleteInteraction.User)))
+            return Task.FromResult(AutocompletionResult.FromError(InteractionCommandError.Unsuccessful,
+                "You don't have permission to view chat triggers!"));
+
+        var inpt = autocompleteInteraction.Data.Current.Value as string;
+
+
+        return Task.FromResult(AutocompletionResult.FromSuccess(_triggers.GetChatTriggersFor(context.Guild?.Id)
             .Where(x => (x.Trigger + x.RealName + x.Response).Contains(inpt, StringComparison.OrdinalIgnoreCase))
             .OrderByDescending(x => x.Trigger.StartsWith(inpt, StringComparison.OrdinalIgnoreCase))
             .Take(25)
