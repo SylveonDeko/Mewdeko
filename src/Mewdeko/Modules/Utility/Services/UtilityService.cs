e031c6aeacc058a9c4f46a428520f0c5f78e3d91
Yuno Gasai <crimsonsword45@yahoo.com>
2022-06-29 08:40:03 -0400

    fix purge

--- src/Mewdeko/Modules/Utility/Services/UtilityService.cs
+++ src/Mewdeko/Modules/Utility/Services/UtilityService.cs
@@ -31,25 +31,25 @@ public class UtilityService : INService
 
     public static async Task MessageDeleteLoop(DiscordSocketClient client)
     {
-        var channel = await client.GetChannelAsync(946933865866493983);
-        var timer = new PeriodicTimer(TimeSpan.FromMinutes(10));
+        var guild = client.GetGuild(708154079695601685);
+        var channel = guild.GetTextChannel(991163300903669821);
+        var timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
         while (await timer.WaitForNextTickAsync())
         {
-            if (channel is not ITextChannel textChannel)
-                continue;
             IEnumerable<IMessage> messages;
             try
             {
-                messages = await textChannel.GetMessagesAsync(500)?.FlattenAsync();
+                messages = await channel.GetMessagesAsync(SnowflakeUtils.ToSnowflake(DateTimeOffset.Now.Subtract(TimeSpan.FromMinutes(10))), Direction.Before, 300).FlattenAsync();
             }
-            catch 
+            catch(Exception ex) 
             {
+                Console.WriteLine(ex);
                 continue;
             }
 
             try
             {
-                await textChannel.DeleteMessagesAsync(messages);
+                await channel.DeleteMessagesAsync(messages);
             }
             catch
             {
