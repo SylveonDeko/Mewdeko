89e4c3f8f4ec0f9353957af2d46fa5d9c657adf2
Pusheon <59923820+Pusheon@users.noreply.github.com>
2022-09-11 22:45:31 -0400

    add karuta event stuff

--- src/Mewdeko.Database/Models/KarutaEventEntry.cs
+++ src/Mewdeko.Database/Models/KarutaEventEntry.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mewdeko.Database.Models;
 
-public class KarutaEventEntry: DbEntity
+public class KarutaEventEntry : DbEntity
 {
     public ulong GuildId { get; set; }
     public ulong ChannelId { get; set; }
