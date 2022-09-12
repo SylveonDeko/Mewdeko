5f8cfd8bdff7a4eb2fb06f5588095c5531ddf21a
Pusheon <59923820+Pusheon@users.noreply.github.com>
2022-09-11 22:45:31 -0400

    add karuta event stuff

--- src/Mewdeko/Modules/Karuta/Services/ShibaKarutaService.cs
+++ src/Mewdeko/Modules/Karuta/Services/ShibaKarutaService.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using LinqToDB.Reflection;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Embed = Discord.Embed;
 
 namespace Mewdeko.Modules.Karuta.Services;
 
@@ -8,14 +12,91 @@ public class ShibaKarutaService : INService
     private readonly ConcurrentDictionary<ulong, int> _messagesSent1 = new();
     private readonly DbService _db;
     private readonly GuildSettingsService _guildSettingsService;
-    public ShibaKarutaService(EventHandler handler, DbService db, GuildSettingsService guildSettingsService)
+    private readonly HttpClient _httpClient;
+    private readonly DiscordSocketClient _client;
+    public ShibaKarutaService(EventHandler handler, DbService db, GuildSettingsService guildSettingsService,
+        HttpClient httpClient,
+        DiscordSocketClient client)
     {
         _db = db;
         _guildSettingsService = guildSettingsService;
+        _httpClient = httpClient;
+        _client = client;
         handler.MessageReceived += GrantKarutaRole;
         handler.MessageReceived += GrantKarutaRole1;
+        handler.MessageReceived += RepostKarutaImage;
     }
-    
+
+    private async Task RepostKarutaImage(SocketMessage args)
+    {
+        if (args.Channel is IDMChannel)
+            return;
+
+        var channel = args.Channel as ITextChannel;
+        var eventChannel = await GetEventChannel(channel.GuildId);
+        if (channel.Id != eventChannel)
+            return;
+
+        if (!args.Attachments.Any()) return;
+        using var sr = await _httpClient.GetAsync(args.Attachments.First().Url, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+        var imgData = await sr.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+        var imgStream = imgData.ToStream();
+        await using var _ = imgStream.ConfigureAwait(false);
+        await using var uow = _db.GetDbContext();
+        await args.DeleteAsync();
+        var entries = uow.KarutaEventEntry.AsQueryable().Where(x => x.GuildId == channel.GuildId).ToList()/*I fucking hate SQLite.*/.LastOrDefault();
+        var entrynum = 1;
+        if (entries is not null)
+        {
+            entrynum = entries.EntryNumber + 1;
+            var componentBuilder = new ComponentBuilder();
+            for (var i = 1; i < 7; i++)
+            {
+                var text = await GetButtonText(channel.GuildId, i);
+                if (string.IsNullOrEmpty(text))
+                    continue;
+                componentBuilder.WithButton(text, $"karutaeventbutton:{i}");
+            }
+
+            var msg = await channel.SendFileAsync(imgStream, filename: args.Attachments.First().Filename,
+                embed: new EmbedBuilder().WithOkColor().WithTitle($"Entry #{entrynum}").WithImageUrl($"attachment://{args.Attachments.First().Filename}").Build(),
+                components: componentBuilder.Build());                           
+            var toadd = new KarutaEventEntry
+            {
+                ChannelId = eventChannel,
+                GuildId = channel.GuildId,
+                EntryNumber = entrynum,
+                MessageId = msg.Id
+            };
+            await uow.KarutaEventEntry.AddAsync(toadd);
+            await uow.SaveChangesAsync();
+        }
+        else
+        {
+            var componentBuilder = new ComponentBuilder();
+            for (var i = 1; i < 7; i++)
+            {
+                var text = await GetButtonText(channel.GuildId, i);
+                if (string.IsNullOrEmpty(text))
+                    continue;
+                componentBuilder.WithButton(text, $"karutaeventbutton:{i}");
+            }
+
+            var msg = await channel.SendFileAsync(imgStream, filename: args.Attachments.First().Filename,
+                embed: new EmbedBuilder().WithOkColor().WithTitle($"Entry #{entrynum}").WithImageUrl($"attachment://{args.Attachments.First().Filename}").Build(),
+                components: componentBuilder?.Build());                           
+            var toadd = new KarutaEventEntry
+            {
+                ChannelId = eventChannel,
+                GuildId = channel.GuildId,
+                EntryNumber = entrynum,
+                MessageId = msg.Id
+            };
+            await uow.KarutaEventEntry.AddAsync(toadd);
+            await uow.SaveChangesAsync();
+        }
+    }
+
     private async Task GrantKarutaRole(IMessage arg)
 
     {
@@ -95,16 +176,138 @@ public class ShibaKarutaService : INService
     public async Task SetButtonText(ulong guildId, int buttonNumber, string text)
     {
         await using var uow = _db.GetDbContext();
+        var get = await uow.KarutaButtonOptions.FirstOrDefaultAsync(x => x.GuildId == guildId);
+        var toadd = get ?? new KarutaButtonOptions { GuildId = guildId };
+
         switch (buttonNumber)
         {
             case 1:
-                var get = uow.KarutaButtonOptions.FirstOrDefault(x => x.GuildId == guildId);
-                if (get is null)
-                {
-                    var toadd = new KarutaButtonOptions { GuildId = guildId, Button1Text = text };
-                }
-
+                toadd.Button1Text = text;
                 break;
+            case 2:
+                toadd.Button2Text = text;
+                break;
+            case 3:
+                toadd.Button3Text = text;
+                break;
+            case 4:
+                toadd.Button4Text = text;
+                break;
+            case 5:
+                toadd.Button5Text = text;
+                break;
+            case 6:
+                toadd.Button6Text = text;
+                break;
+            
         }
+        
+        uow.KarutaButtonOptions.Update(toadd);
+        await uow.SaveChangesAsync();
     }
+
+    public async Task ClearEntries(ulong guildId)
+    {
+        await using var uow = _db.GetDbContext();
+        var entries = uow.KarutaEventEntry.Where(x => x.GuildId == guildId);
+        var votes = uow.KarutaEventVotes.Where(x => x.GuildId == guildId);
+        if (entries.Any())
+            uow.KarutaEventEntry.RemoveRange(entries);
+        if (votes.Any())
+            uow.KarutaEventVotes.RemoveRange(votes);
+        await uow.SaveChangesAsync();
+
+    }
+
+    public async Task<bool> RemoveEntry(ulong guildId, int entryNum)
+    {
+        await using var uow = _db.GetDbContext();
+        var entry = uow.KarutaEventEntry.FirstOrDefault(x => x.GuildId == guildId && x.EntryNumber == entryNum);
+        if (entry is null)
+            return false;
+        var votes = uow.KarutaEventVotes.Where(x => x.GuildId == guildId && x.MessageId == entry.MessageId);
+        uow.KarutaEventEntry.Remove(entry);
+        if (votes.Any())
+            uow.KarutaEventVotes.RemoveRange(votes);
+        await uow.SaveChangesAsync();
+        var channelId = await GetEventChannel(guildId);
+        var channel = _client.GetChannel(channelId) as SocketTextChannel;
+        var message = await channel.GetMessageAsync(entry.MessageId);
+        try
+        {
+            await message.DeleteAsync();
+        }
+        catch
+        {
+           // ignored
+        }
+        return true;
+    }
+
+    public async Task<string> GetButtonText(ulong guildId, int button)
+    {
+        await using var uow = _db.GetDbContext();
+        var get = await uow.KarutaButtonOptions.FirstOrDefaultAsync(x => x.GuildId == guildId);
+        if (get is null)
+            return string.Empty;
+
+        return button switch
+        {
+            1 => get.Button1Text,
+            2 => get.Button2Text,
+            3 => get.Button3Text,
+            4 => get.Button4Text,
+            5 => get.Button5Text,
+            6 => get.Button6Text,
+            _ => string.Empty
+        };
+    }
+
+    public async Task<Embed?> GetLeaderboardEmbed(ulong guildId, int voteNum)
+    {
+        await using var uow = _db.GetDbContext();
+        var buttonText = await GetButtonText(guildId, voteNum);
+        IEnumerable<KarutaEventEntry> entries;
+        if (string.IsNullOrEmpty(buttonText))
+            return null;
+        switch (voteNum)
+        {
+            case 1:
+                entries = uow.KarutaEventEntry.ToList().Where(x => x.GuildId == guildId).OrderByDescending(x => x.Button1Count).Take(8);
+                return new EmbedBuilder().WithOkColor().WithTitle($"Top 8 for {buttonText}").WithDescription(entries.Any()
+                    ? string.Join("\n", entries.Select(x => $"**[Entry {x.EntryNumber} - {x.Button1Count}]({GetJumpUrl(x.GuildId, x.ChannelId, x.MessageId)})**"))
+                    : "**No recorded entries yet.**").Build();
+            case 2:
+                entries = uow.KarutaEventEntry.ToList().Where(x => x.GuildId == guildId).OrderByDescending(x => x.Button2Count).Take(8);
+                return new EmbedBuilder().WithOkColor().WithTitle($"Top 8 for {buttonText}").WithDescription(entries.Any()
+                    ? string.Join("\n", entries.Select(x => $"**[Entry {x.EntryNumber} - {x.Button2Count}]({GetJumpUrl(x.GuildId, x.ChannelId, x.MessageId)})**"))
+                    : "**No recorded entries yet.**").Build();
+            case 3:
+                entries = uow.KarutaEventEntry.ToList().Where(x => x.GuildId == guildId).OrderByDescending(x => x.Button3Count).Take(8);
+                return new EmbedBuilder().WithOkColor().WithTitle($"Top 8 for {buttonText}").WithDescription(entries.Any()
+                    ? string.Join("\n", entries.Select(x => $"**[Entry {x.EntryNumber} - {x.Button3Count}]({GetJumpUrl(x.GuildId, x.ChannelId, x.MessageId)})**"))
+                    : "**No recorded entries yet.**").Build();
+            case 4:
+                entries = uow.KarutaEventEntry.ToList().Where(x => x.GuildId == guildId).OrderByDescending(x => x.Button4Count).Take(8);
+                return new EmbedBuilder().WithOkColor().WithTitle($"Top 8 for {buttonText}").WithDescription(entries.Any()
+                    ? string.Join("\n", entries.Select(x => $"**[Entry {x.EntryNumber} - {x.Button4Count}]({GetJumpUrl(x.GuildId, x.ChannelId, x.MessageId)})**"))
+                    : "**No recorded entries yet.**").Build();
+            case 5:
+                entries = uow.KarutaEventEntry.ToList().Where(x => x.GuildId == guildId).OrderByDescending(x => x.Button5Count).Take(8);
+                return new EmbedBuilder().WithOkColor().WithTitle($"Top 8 for {buttonText}").WithDescription(entries.Any()
+                    ? string.Join("\n", entries.Select(x => $"**[Entry {x.EntryNumber} - {x.Button5Count}]({GetJumpUrl(x.GuildId, x.ChannelId, x.MessageId)})**"))
+                    : "**No recorded entries yet.**").Build();
+            case 6:
+                entries = uow.KarutaEventEntry.ToList().Where(x => x.GuildId == guildId).OrderByDescending(x => x.Button6Count).Take(8);
+                return new EmbedBuilder().WithOkColor().WithTitle($"Top 8 for {buttonText}").WithDescription(entries.Any()
+                    ? string.Join("\n", entries.Select(x => $"**[Entry {x.EntryNumber} - {x.Button6Count}]({GetJumpUrl(x.GuildId, x.ChannelId, x.MessageId)})**"))
+                    : "**No recorded entries yet.**").Build();
+            default:
+                return null;
+        }
+        
+    }
+        
+    public static string GetJumpUrl(ulong guildId, ulong channelId, ulong messageId)
+        => $"https://discord.com/channels/{guildId}/{channelId}/{messageId}";
 }
