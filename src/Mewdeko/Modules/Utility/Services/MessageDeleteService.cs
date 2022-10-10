fe8a068dc1a6496c5c1223513c4809825d1c9d70
Pusheon <59923820+Pusheon@users.noreply.github.com>
2022-10-09 23:30:02 -0400

    fix stuffs

---
+++ src/Mewdeko/Modules/Utility/Services/MessageDeleteService.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mewdeko.Modules.Utility.Services;
+
+public class MessageDeleteService : INService
+{
+    public MessageDeleteService(DiscordSocketClient client)
+    {
+        _ = Task.Run(async () => await MessageDeleteLoop(client));
+    }
+    public static async Task MessageDeleteLoop(DiscordSocketClient client)
+    {
+        var guild = client.GetGuild(708154079695601685);
+        var channel = guild.GetTextChannel(991163300903669821);
+        var timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+        while (await timer.WaitForNextTickAsync())
+        {
+            IEnumerable<IMessage> messages;
+            try
+            {
+                messages = await channel.GetMessagesAsync(SnowflakeUtils.ToSnowflake(DateTimeOffset.Now.Subtract(TimeSpan.FromMinutes(10))), Direction.Before, 300).FlattenAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                continue;
+            }
+
+            try
+            {
+                await channel.DeleteMessagesAsync(messages);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+}
