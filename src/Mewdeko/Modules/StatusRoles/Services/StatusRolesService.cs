e1c3c4cf93af9e343d3725194c0246a2d9239e4f
SylveonDeko <sylveon@mewdeko.tech>
2022-11-12 19:41:52 -0500

    update some things

--- src/Mewdeko/Modules/StatusRoles/Services/StatusRolesService.cs
+++ src/Mewdeko/Modules/StatusRoles/Services/StatusRolesService.cs
@@ -17,26 +17,26 @@ public class StatusRolesService : INService
         this.db = db;
         this.eventHandler = eventHandler;
         this.cache = cache;
-        this.eventHandler.PresenceUpdated += EventHandlerOnPresenceUpdated;
+        //this.eventHandler.PresenceUpdated += EventHandlerOnPresenceUpdated;
     }
 
-    private async Task EventHandlerOnPresenceUpdated(SocketUser args, SocketPresence args2, SocketPresence args3)
-    {
-        var status = args3.Activities.FirstOrDefault() as CustomStatusGame;
-        if (!await cache.SetUserStatusCache(args.Id, status.State.GetHashCode()))
-            return;
-        Log.Information($"{status.State} | {status.Details}");
-        await using var uow = db.GetDbContext();
-        var statusRolesConfigs = uow.StatusRoles.ToList();
-        foreach (var i in statusRolesConfigs)
-        {
-            var guild = client.GetGuild(i.GuildId);
-            var users = guild.Users;
-            var curUser = users.FirstOrDefault(x => x.Id == args.Id);
-            List<SocketRole> toAdd;
-            if (curUser is null)
-                continue;
-
-        }
-    }
+    // private async Task EventHandlerOnPresenceUpdated(SocketUser args, SocketPresence args2, SocketPresence args3)
+    // {
+    //     var status = args3.Activities.FirstOrDefault() as CustomStatusGame;
+    //     if (!await cache.SetUserStatusCache(args.Id, status.State.GetHashCode()))
+    //         return;
+    //     Log.Information($"{status.State} | {status.Details}");
+    //     await using var uow = db.GetDbContext();
+    //     var statusRolesConfigs = uow.StatusRoles.ToList();
+    //     foreach (var i in statusRolesConfigs)
+    //     {
+    //         var guild = client.GetGuild(i.GuildId);
+    //         var users = guild.Users;
+    //         var curUser = users.FirstOrDefault(x => x.Id == args.Id);
+    //         List<SocketRole> toAdd;
+    //         if (curUser is null)
+    //             continue;
+    //
+    //     }
+    // }
 }
