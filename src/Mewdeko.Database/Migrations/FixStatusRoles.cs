1568b71ea476481104960fd339a9410b823774a3
SylveonDeko <sylveon@mewdeko.tech>
2022-12-29 12:06:09 -0500

    more statusroles work, added createroles command for easy role management

---
+++ src/Mewdeko.Database/Migrations/FixStatusRoles.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mewdeko.Database.Migrations;
+
+public partial class FixStatusRoles : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>("StatusEmbed", "StatusRoles", nullable: true);
+        migrationBuilder.AddColumn<string>("StatusChannelId", "StatusRoles", nullable: true);
+        migrationBuilder.AddColumn<bool>("ReaddRemoved", "StatusRoles", defaultValue: false);
+        migrationBuilder.AddColumn<string>("RemoveAdded", "StatusRoles", defaultValue: true);
+    }
+}
