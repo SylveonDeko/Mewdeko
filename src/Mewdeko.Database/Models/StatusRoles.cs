04aea0a7dc885a9cc7f4252078342de0accb3263
SylveonDeko <sylveon@mewdeko.tech>
2022-12-29 12:06:09 -0500

    more statusroles work, added createroles command for easy role management

--- src/Mewdeko.Database/Models/StatusRoles.cs
+++ src/Mewdeko.Database/Models/StatusRoles.cs
@@ -6,6 +6,8 @@ public class StatusRoles : DbEntity
     public string Status { get; set; }
     public string ToAdd { get; set; }
     public string ToRemove { get; set; }
-    public string StatusEmbed { get; set; }
+    public string StatusEmbed { get; set; } = null;
     public bool ReaddRemoved { get; set; } = false;
+    public bool RemoveAdded { get; set; } = true;
+    public ulong StatusChannelId { get; set; }
 }
