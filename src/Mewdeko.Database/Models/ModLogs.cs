228edf063575059739abd84daf0cd915a9ca6b8c
SylveonDeko <sylveon@mewdeko.tech>
2023-01-13 07:55:35 -0500

    i made my changes for the day <a:furretwalk:927743575238795286>

---
+++ src/Mewdeko.Database/Models/ModLogs.cs
@@ -0,0 +1,10 @@
+﻿namespace Mewdeko.Database.Models;
+
+public class ModLogs : DbEntity
+{
+    public ulong GuildId { get; set; }
+    public ulong ChannelId { get; set; }
+    public ulong UserId { get; set; }
+    public string Action { get; set; }
+    public string Reason { get; set; }
+}
