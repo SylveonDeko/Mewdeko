daebc9fb680e3f49167496726c4ce0b52b6f9e28
SylveonDeko <sylveon@mewdeko.tech>
2023-01-13 07:55:35 -0500

    i made my changes for the day <a:furretwalk:927743575238795286>

---
+++ src/Mewdeko.Database/Models/GuildCurrency.cs
@@ -0,0 +1,8 @@
+﻿namespace Mewdeko.Database.Models;
+
+public class GuildCurrency
+{
+    public ulong GuildId { get; set; }
+    public ulong UserId { get; set; }
+    public int Amount { get; set; }
+}
