de4b92481fc7e0532debeb3a82d9eff44133f113
cat <lumitydev@gmail.com>
2023-02-05 20:03:18 -0600

    masskick

---
+++ src/Mewdeko/Common/TypeReaders/UlongArrayTypeReader.cs
@@ -0,0 +1,22 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord.Commands;
+using MoreLinq.Extensions;
+
+namespace Mewdeko.Common.TypeReaders;
+
+public class UlongArrayTypeReader : MewdekoTypeReader<ulong[]>
+{
+
+    public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider _)
+    {
+        var inputs = input.Split(' ', ',', '.', ':', '-');
+        var data = inputs.Where(x => !x.IsNullOrWhiteSpace()).Select(x => Convert.ToUInt64(x)).ToList();
+
+        return Task.FromResult(data != null ? TypeReaderResult.FromSuccess(data) : TypeReaderResult.FromError(CommandError.ParseFailed, "obonobo"));
+    }
+
+    public UlongArrayTypeReader(DiscordSocketClient client, CommandService cmds) : base(client, cmds)
+    {
+    }
+}
