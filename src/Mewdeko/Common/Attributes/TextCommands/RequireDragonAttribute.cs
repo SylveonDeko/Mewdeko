6f82b1cb8456fa55d253ccb8b4e9e2f28e4ef8f0
SylveonDeko <crimsonslayer76@gmail.com>
2023-02-15 09:48:13 -0500

    remove useless dependencies, update to net7, fix dragon attrib

--- src/Mewdeko/Common/Attributes/TextCommands/RequireDragonAttribute.cs
+++ src/Mewdeko/Common/Attributes/TextCommands/RequireDragonAttribute.cs
@@ -8,19 +8,8 @@ public class RequireDragonAttribute : PreconditionAttribute
 {
     public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
     {
-        // always return true in precondition checks to avoid incorrect error messages
-        var isTest = true;
-        foreach (var alias in command.Aliases)
-        {
-            if (context.Message.Content.Contains(alias))
-                isTest = false;
-        }
-
-        if (isTest)
-            return PreconditionResult.FromSuccess();
-
-
         var db = services.GetRequiredService(typeof(DbService)) as DbService;
+        var guildConfigService = services.GetRequiredService(typeof(GuildSettingsService)) as GuildSettingsService;
         await using var ctx = db.GetDbContext();
         var user = await ctx.GetOrCreateUser(context.User);
         return user.IsDragon
@@ -28,6 +17,6 @@ public class RequireDragonAttribute : PreconditionAttribute
             : PreconditionResult.FromError("Your meek human arms could never push the 10,000 pound rock blocking the " +
                                            "path out of the cave of stable features. You must call upon the dragon in " +
                                            "your soul to open a passage into the abyss of new features. (enable beta " +
-                                           "mode by running `.dragon` to use this command)");
+                                           $"mode by running `{await guildConfigService.GetPrefix(context.Guild?.Id)}dragon` to use this command)");
     }
 }
