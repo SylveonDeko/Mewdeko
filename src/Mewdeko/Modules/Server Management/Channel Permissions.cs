b8f7315c0c753147c4e82227adbf8834a49dfb0f
sylveondeko <oksamit45@gmail.com>
2023-04-25 23:35:07 -0400

    Fixed greroll, added the ability to set permissions to inherit for channels

--- src/Mewdeko/Modules/Server Management/Channel Permissions.cs
+++ src/Mewdeko/Modules/Server Management/Channel Permissions.cs
@@ -1,4 +1,3 @@
-using System.Threading.Tasks;
 using Discord.Commands;
 using Mewdeko.Common.Attributes.TextCommands;
 
@@ -12,66 +11,120 @@ public partial class ServerManagement
             => permissions.Aggregate<ChannelPermission, ulong>(0, (current, permission) => current | (ulong)permission);
 
         [Cmd, Aliases, RequireContext(ContextType.Guild),
-         UserPerm(GuildPermission.ManageChannels), Priority(0)]
+         UserPerm(GuildPermission.ManageChannels), Priority(1)]
         public async Task PermControl(SocketGuildChannel channel, PermValue perm, IRole role,
             params ChannelPermission[] perms)
         {
             OverwritePermissions result;
             var newPermsRaw = GetRawPermissionValue(perms);
             var currentPerms = channel.GetPermissionOverwrite(role);
+
             if (currentPerms == null)
             {
+                if (perm == PermValue.Inherit) return;
                 result = perm == PermValue.Allow ? new OverwritePermissions(newPermsRaw, 0) : new OverwritePermissions(0, newPermsRaw);
             }
             else
             {
                 var allowPermsRaw = GetRawPermissionValue(currentPerms.Value.ToAllowList());
                 var denyPermsRaw = GetRawPermissionValue(currentPerms.Value.ToDenyList());
-                if (perm == PermValue.Allow)
-                    allowPermsRaw |= newPermsRaw;
-                else
-                    denyPermsRaw |= newPermsRaw;
+
+                switch (perm)
+                {
+                    case PermValue.Allow:
+                        allowPermsRaw |= newPermsRaw;
+                        denyPermsRaw &= ~newPermsRaw;
+                        break;
+                    case PermValue.Deny:
+                        denyPermsRaw |= newPermsRaw;
+                        allowPermsRaw &= ~newPermsRaw;
+                        break;
+                    default:
+                        allowPermsRaw &= ~newPermsRaw;
+                        denyPermsRaw &= ~newPermsRaw;
+                        break;
+                }
+
                 result = new OverwritePermissions(allowPermsRaw, denyPermsRaw);
             }
 
             var list = perms.Select(e => e.ToString()).ToList();
             await channel.AddPermissionOverwriteAsync(role, result).ConfigureAwait(false);
-            await ctx.Channel.SendConfirmAsync(
-                $"I have allowed the following permissions for the role {role.Mention} in {channel}: \n**{string.Join("\n", list)}**").ConfigureAwait(false);
+
+            switch (perm)
+            {
+                case PermValue.Inherit:
+                    await ctx.Channel.SendConfirmAsync($"I have set the following permissions for the user {role.Mention} in {channel} to inherit: \n**{string.Join("\n", list)}**")
+                        .ConfigureAwait(false);
+                    break;
+                case PermValue.Allow:
+                    await ctx.Channel.SendConfirmAsync($"I have allowed the following permissions for the user {role.Mention} in {channel}: \n**{string.Join("\n", list)}**")
+                        .ConfigureAwait(false);
+                    break;
+                default:
+                    await ctx.Channel.SendConfirmAsync($"I have denied the following permissions for the user {role.Mention} in {channel}: \n**{string.Join("\n", list)}**")
+                        .ConfigureAwait(false);
+                    break;
+            }
         }
+
 
         [Cmd, Aliases, RequireContext(ContextType.Guild),
          UserPerm(GuildPermission.ManageChannels), Priority(1)]
-        public async Task PermControl(SocketGuildChannel channel, PermValue perm, IUser role,
+        public async Task PermControl(SocketGuildChannel channel, PermValue perm, IUser user,
             params ChannelPermission[] perms)
         {
-            var list = new List<string>();
             OverwritePermissions result;
             var newPermsRaw = GetRawPermissionValue(perms);
-            var currentPerms = channel.GetPermissionOverwrite(role);
+            var currentPerms = channel.GetPermissionOverwrite(user);
+
             if (currentPerms == null)
             {
+                if (perm == PermValue.Inherit) return;
                 result = perm == PermValue.Allow ? new OverwritePermissions(newPermsRaw, 0) : new OverwritePermissions(0, newPermsRaw);
             }
             else
             {
                 var allowPermsRaw = GetRawPermissionValue(currentPerms.Value.ToAllowList());
                 var denyPermsRaw = GetRawPermissionValue(currentPerms.Value.ToDenyList());
-                if (perm == PermValue.Allow)
-                    allowPermsRaw |= newPermsRaw;
-                else
-                    denyPermsRaw |= newPermsRaw;
+
+                switch (perm)
+                {
+                    case PermValue.Allow:
+                        allowPermsRaw |= newPermsRaw;
+                        denyPermsRaw &= ~newPermsRaw;
+                        break;
+                    case PermValue.Deny:
+                        denyPermsRaw |= newPermsRaw;
+                        allowPermsRaw &= ~newPermsRaw;
+                        break;
+                    default:
+                        allowPermsRaw &= ~newPermsRaw;
+                        denyPermsRaw &= ~newPermsRaw;
+                        break;
+                }
+
                 result = new OverwritePermissions(allowPermsRaw, denyPermsRaw);
             }
 
-            foreach (var e in perms) list.Add(e.ToString());
-            await channel.AddPermissionOverwriteAsync(role, result).ConfigureAwait(false);
-            if (perm == PermValue.Allow)
-                await ctx.Channel.SendConfirmAsync($"I have allowed the following permissions for the user {role.Mention} in {channel}: \n**{string.Join("\n", list)}**")
-                    .ConfigureAwait(false);
-            else
-                await ctx.Channel.SendConfirmAsync($"I have denied the following permissions for the user {role.Mention} in {channel}: \n**{string.Join("\n", list)}**")
-                    .ConfigureAwait(false);
+            var list = perms.Select(e => e.ToString()).ToList();
+            await channel.AddPermissionOverwriteAsync(user, result).ConfigureAwait(false);
+
+            switch (perm)
+            {
+                case PermValue.Inherit:
+                    await ctx.Channel.SendConfirmAsync($"I have set the following permissions for the user {user.Mention} in {channel} to inherit: \n**{string.Join("\n", list)}**")
+                        .ConfigureAwait(false);
+                    break;
+                case PermValue.Allow:
+                    await ctx.Channel.SendConfirmAsync($"I have allowed the following permissions for the user {user.Mention} in {channel}: \n**{string.Join("\n", list)}**")
+                        .ConfigureAwait(false);
+                    break;
+                default:
+                    await ctx.Channel.SendConfirmAsync($"I have denied the following permissions for the user {user.Mention} in {channel}: \n**{string.Join("\n", list)}**")
+                        .ConfigureAwait(false);
+                    break;
+            }
         }
     }
 }
