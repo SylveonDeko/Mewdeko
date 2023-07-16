b8bfdf5b620a9a8d837c6cfa53a201b6ab331fd7
SylveonDeko <sylveon@mewdeko.tech>
2023-07-16 14:48:53 -0400

    add the ability to recover your server if you lose access to the owner account by granting an admin role of the highest power mewdeko can muster, add leavestats command

---
+++ src/Mewdeko/Common/Modals/RecoveryKeyModal.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Interactions;
+
+namespace Mewdeko.Common.Modals;
+
+public class RecoveryKeyModal : IModal
+{
+    public string Title => "Recovery Key";
+
+    [InputLabel("Recovery Key")]
+    [ModalTextInput("recovery-key", TextInputStyle.Paragraph, "Please enter your Recovery Key"), RequiredInput(false)]
+    public string RecoveryKey { get; set; }
+}
