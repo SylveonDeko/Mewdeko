03aa4a91570d352fa8f25958a47bfc34467026cc
sylv <sylveon@mewdeko.tech>
2023-09-05 02:32:37 -0400

    brain errors

--- src/Mewdeko/Services/CommandHandler.cs
+++ src/Mewdeko/Services/CommandHandler.cs
@@ -559,7 +559,7 @@ public class CommandHandler : INService
                 }
                 catch (Exception e)
                 {
-                    Log.Error("Error occured in the handler:", e);
+                    Log.Error($"Error occured in the handler: {e}");
                 }
             }
 
