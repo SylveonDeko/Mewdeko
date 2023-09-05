c898ff3e96f5f102ae3c6b2e98051a241956ab22
sylv <sylveon@mewdeko.tech>
2023-09-05 02:30:29 -0400

    errors

--- src/Mewdeko/Services/CommandHandler.cs
+++ src/Mewdeko/Services/CommandHandler.cs
@@ -382,16 +382,6 @@ public class CommandHandler : INService
 #endif
     }
 
-    public string SetDefaultPrefix(string prefix)
-    {
-        if (string.IsNullOrWhiteSpace(prefix))
-            throw new ArgumentNullException(nameof(prefix));
-
-        bss.ModifyConfig(bs => bs.Prefix = prefix);
-
-        return prefix;
-    }
-
 
     public void AddServices(IServiceCollection services)
     {
@@ -563,7 +553,14 @@ public class CommandHandler : INService
         {
             while (CommandParseQueue[channelId].TryDequeue(out var msg))
             {
-                await TryRunCommand((msg.Channel as IGuildChannel)?.Guild, msg.Channel, msg).ConfigureAwait(false);
+                try
+                {
+                    await TryRunCommand((msg.Channel as IGuildChannel)?.Guild, msg.Channel, msg).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    Log.Error("Error occured in the handler:", e);
+                }
             }
 
             CommandParseQueue[channelId] = new ConcurrentQueue<IUserMessage>();
