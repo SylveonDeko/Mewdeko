82f7045bf17d99320fcc998a4e8542812b4db41e
Sean <sean.daxxtrias@gmail.com>
2024-01-06 15:06:57 -0600

    bracket orders decomissioned, so disabling link to them

--- src/Mewdeko/Modules/Utility/SlashUtility.cs
+++ src/Mewdeko/Modules/Utility/SlashUtility.cs
@@ -236,7 +236,7 @@ public class SlashUtility : MewdekoSlashModuleBase<UtilityService>
             "bitget" or "bg" => "https://docs.tealstreet.io/docs/connect/bitget",
             "bitmex" or "bmex" => "https://docs.tealstreet.io/docs/connect/bitmex",
             "bybit" => "https://docs.tealstreet.io/docs/connect/bybit",
-            "brackets" or "bracketorders" or "bracket-orders" => "https://docs.tealstreet.io/docs/trade/bracket-orders",
+            //"brackets" or "bracketorders" or "bracket-orders" => "https://docs.tealstreet.io/docs/trade/bracket-orders",
             "bybitv5" => "https://docs.tealstreet.io/docs/connect/bybitv5",
             "bingx" => "https://docs.tealstreet.io/docs/connect/bingx",
             "cli" or "console" or "commandline" => "https://docs.tealstreet.io/docs/cli",
