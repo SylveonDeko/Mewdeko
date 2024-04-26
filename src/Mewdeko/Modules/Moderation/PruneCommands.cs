499412ac3ee78e94fa377b14e4c22a3581e0c929
sylveondeko <sylveon@mewdeko.tech>
2024-04-26 11:52:01 -0400

    Added purgeuser, purge now accepts more than one input for parameters

--- src/Mewdeko/Modules/Moderation/PruneCommands.cs
+++ src/Mewdeko/Modules/Moderation/PruneCommands.cs
@@ -1,6 +1,5 @@
 ﻿using Discord.Commands;
 using Mewdeko.Common.Attributes.TextCommands;
-using Mewdeko.Common.TypeReaders.Models;
 using Mewdeko.Modules.Moderation.Services;
 
 namespace Mewdeko.Modules.Moderation;
@@ -58,104 +57,84 @@ public partial class Moderation
         /// </code>
         /// </summary>
         /// <param name="count">The amount of messages to purge</param>
-        /// <param name="parameter">The parameter to use</param>
-        /// <param name="input">The extra input for a parameter if needed</param>
+        /// <param name="parameters">The parameters to use</param>
         [Cmd, Aliases, RequireContext(ContextType.Guild),
          UserPerm(ChannelPermission.ManageMessages), BotPerm(ChannelPermission.ManageMessages), Priority(1)]
-        public async Task Purge(ulong count, string? parameter = null, string? input = null)
+        public async Task Purge(ulong count, string? parameters = null)
         {
             if (await client.Rest.GetUserAsync(count) is not null)
             {
-                await Purge(count, 100, parameter);
+                await Purge(count, 100, parameters);
                 return;
             }
 
-            StoopidTime? time = null;
-            try
+            if (parameters is null)
             {
-                time = StoopidTime.FromInput(input);
-            }
-            catch (ArgumentException)
-            {
-                //ignore
+                await Service.PurgeWhere(ctx.Channel as ITextChannel, count, x => x.Channel.Id == ctx.Channel.Id);
+                return;
             }
 
-            count++;
-            switch (count)
+            var options = new List<Func<IMessage, bool>>();
+
+            var parts = parameters.Split(new[]
             {
-                case < 1:
-                    return;
-                case > 1000:
-                    count = 1000;
-                    break;
+                ' ', ','
+            }, StringSplitOptions.RemoveEmptyEntries);
+            string input = null;
+
+            for (var i = 0; i < parts.Length; i++)
+            {
+                switch (parts[i])
+                {
+                    case "-s":
+                    case "--safe":
+                        options.Add(x => !x.IsPinned);
+                        break;
+                    case "-nb":
+                    case "--nobots":
+                        options.Add(x => !x.Author.IsBot);
+                        break;
+                    case "-ob":
+                    case "--onlybots":
+                        options.Add(x => x.Author.IsBot);
+                        break;
+                    case "-he":
+                    case "--hasembed":
+                        options.Add(x => x.Embeds.Count > 0);
+                        break;
+                    case "-ne":
+                    case "--noembed":
+                        options.Add(x => x.Embeds.Count == 0);
+                        break;
+                    case "-c":
+                    case "--contains":
+                        if (i + 1 < parts.Length) input = parts[++i];
+                        if (!string.IsNullOrEmpty(input))
+                            options.Add(x => x.Content.Contains(input, StringComparison.InvariantCultureIgnoreCase));
+                        break;
+                    case "-b":
+                    case "--before":
+                    case "-a":
+                    case "--after":
+                        if (i + 1 < parts.Length)
+                        {
+                            if (DateTimeOffset.TryParse(parts[++i], out var date))
+                            {
+                                if (parts[i - 1].Contains("before"))
+                                    options.Add(x => x.Timestamp < date);
+                                else
+                                    options.Add(x => x.Timestamp > date);
+                            }
+                        }
+
+                        break;
+                }
             }
 
-            switch (parameter)
-            {
-                case "-s":
-                case "--safe":
-                    await Service.PurgeWhere((ITextChannel)ctx.Channel, count, x => !x.IsPinned)
-                        .ConfigureAwait(false);
-                    return;
-                case "-nb":
-                case "--nobots":
-                    await Service.PurgeWhere((ITextChannel)ctx.Channel, count, x => !x.Author.IsBot)
-                        .ConfigureAwait(false);
-                    return;
-                case "-ob":
-                case "--onlybots":
-                    await Service.PurgeWhere((ITextChannel)ctx.Channel, count, x => x.Author.IsBot)
-                        .ConfigureAwait(false);
-                    break;
-                case "-b":
-                case "--before":
-                    if (time is null)
-                        return;
-                    if (time.Time > TwoWeeks)
-                        return;
-                    await Service.PurgeWhere((ITextChannel)ctx.Channel, count,
-                            x => DateTimeOffset.Now.Subtract(x.Timestamp).TotalSeconds <= time.Time.TotalSeconds)
-                        .ConfigureAwait(false);
-                    break;
-                case "-a":
-                case "--after":
-                    if (time is null)
-                        return;
-                    if (time.Time > TwoWeeks)
-                        return;
-                    await Service.PurgeWhere((ITextChannel)ctx.Channel, count,
-                            x => DateTimeOffset.Now.Subtract(x.Timestamp).TotalSeconds >= time.Time.TotalSeconds)
-                        .ConfigureAwait(false);
-                    break;
-                case "-he":
-                case "--hasembed":
-                    await Service.PurgeWhere((ITextChannel)ctx.Channel, count, x => x.Embeds.Count > 0)
-                        .ConfigureAwait(false);
-                    break;
-                case "-ne":
-                case "--noembed":
-                    await Service.PurgeWhere((ITextChannel)ctx.Channel, count, x => x.Embeds.Count == 0)
-                        .ConfigureAwait(false);
-                    break;
-                case "-c":
-                case "--contains":
-                    if (input is null)
-                        return;
-                    await Service.PurgeWhere((ITextChannel)ctx.Channel, count,
-                        x => x.Content.ToLowerInvariant().Contains(input)).ConfigureAwait(false);
-                    //     break;
-                    // case "-u":
-                    // case "--until":
-                    //     if (input is null)
-                    //         return;
-                    //     if (!ulong.TryParse(input, out var messageId))
-                    //         return;
-                    //     await Service.PurgeWhere((ITextChannel)ctx.Channel, 0, _ => true, messageId);
-                    break;
-                default:
-                    await Service.PurgeWhere((ITextChannel)ctx.Channel, count, _ => true).ConfigureAwait(false);
-                    break;
-            }
+            await Service.PurgeWhere((ITextChannel)ctx.Channel, count, CombinedPredicate).ConfigureAwait(false);
+            return;
+
+            bool CombinedPredicate(IMessage m) => options.All(p => p(m));
         }
 
         //Purge @user [x]
@@ -170,16 +149,6 @@ public partial class Moderation
          UserPerm(ChannelPermission.ManageMessages), BotPerm(ChannelPermission.ManageMessages), Priority(0)]
         public Task Purge(IGuildUser user, ulong count = 100, string? parameter = null) =>
             Purge(user.Id, count, parameter);
-
-        /// <summary>
-        /// Purges messages from the current channel with the specific parameters.
-        /// </summary>
-        /// <param name="parameter">The parameter to use</param>
-        /// <param name="input">The extra input for a parameter if needed</param>
-        /// <returns></returns>
-        [Cmd, Aliases, RequireContext(ContextType.Guild),
-         UserPerm(ChannelPermission.ManageMessages), BotPerm(ChannelPermission.ManageMessages), Priority(0)]
-        public Task Purge(string? parameter = null, string input = null) => Purge(0, parameter, input);
 
         //Purge userid [x]
         /// <summary>
@@ -213,6 +182,69 @@ public partial class Moderation
 
             return Service.PurgeWhere((ITextChannel)ctx.Channel, count,
                 m => m.Author.Id == userId && DateTime.UtcNow - m.CreatedAt < TwoWeeks);
+        }
+
+        /// <summary>
+        /// Purges all messages from accesible channels for a user.
+        /// </summary>
+        /// <param name="user">The user who's messages to purge</param>
+        /// <param name="messageCount">The count of messages to search</param>
+        [Cmd, Aliases, UserPerm(GuildPermission.ManageMessages), BotPerm(GuildPermission.ManageMessages)]
+        public async Task PurgeUser(IUser user, int messageCount)
+        {
+            if (messageCount is > 1000 or < 1)
+            {
+                await ctx.Channel.SendErrorAsync("Invalid amount specified. Max is 1000, Minimum is 1.", Config);
+                return;
+            }
+
+            await ctx.Channel.SendConfirmAsync(
+                $"Searching the last {messageCount} messages in all channels to purge messages from {user.Mention}....");
+
+            var successCount = 0;
+            var failCount = 0;
+            var deletedMessageCount = 0;
+            var channels = await ctx.Guild.GetTextChannelsAsync();
+            foreach (var i in channels)
+            {
+                try
+                {
+                    var messages = await i.GetMessagesAsync(messageCount).FlattenAsync();
+                    messages = messages.Where(x => x.Author == user);
+
+                    if (messages is null || !messages.Any())
+                        continue;
+
+                    await i.DeleteMessagesAsync(messages);
+                    successCount++;
+                    deletedMessageCount += messages.Count();
+                }
+                catch
+                {
+                    failCount++;
+                }
+            }
+
+            switch (successCount)
+            {
+                case > 0 when failCount is 0:
+                    await ctx.Channel.SendConfirmAsync(
+                        $"Deleted {deletedMessageCount} messages from {user.Mention} in {successCount} channels.");
+                    break;
+                case > 0 when failCount > 0:
+                    await ctx.Channel.SendConfirmAsync(
+                        $"Deleted {deletedMessageCount} messages from {user.Mention} in {successCount} channels." +
+                        $"\n{failCount} channels were unable to be processed due to permission issues.");
+                    break;
+                case 0 when failCount > 0:
+                    await ctx.Channel.SendErrorAsync(
+                        "No messages were processed due to permission issues. Please make sure Mewdeko can see all channels.",
+                        Config);
+                    break;
+                case 0 when failCount is 0:
+                    await ctx.Channel.SendConfirmAsync($"There were no messages to delete from {user.Mention}");
+                    break;
+            }
         }
     }
 }
