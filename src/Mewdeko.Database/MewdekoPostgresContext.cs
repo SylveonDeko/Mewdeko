d3e21d02b4440867c5e3bc3ef5406fc9a00bf795
SylveonDeko <crimsonslayer76@gmail.com>
2024-06-16 13:46:45 -0400

    maybe

--- src/Mewdeko.Database/MewdekoPostgresContext.cs
+++ src/Mewdeko.Database/MewdekoPostgresContext.cs
@@ -14,6 +14,7 @@ public sealed class MewdekoPostgresContext(string connStr = "") : MewdekoContext
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
         optionsBuilder
+            .EnableServiceProviderCaching()
             .EnableDetailedErrors()
             .EnableSensitiveDataLogging()
             .EnableServiceProviderCaching()
