48f0215ceee605c302ca668e6880d54530e93ac6
SylveonDeko <59923820+SylveonDeko@users.noreply.github.com>
2024-10-07 12:54:17 -0400

    oops

--- src/Mewdeko/Modules/Music/SlashMusic.cs
+++ src/Mewdeko/Modules/Music/SlashMusic.cs
@@ -8,6 +8,7 @@ using Lavalink4NET;
 using Lavalink4NET.DiscordNet;
 using Lavalink4NET.Players;
 using Lavalink4NET.Rest.Entities.Tracks;
+using Lavalink4NET.Tracks;
 using Mewdeko.Common.Attributes.InteractionCommands;
 using Mewdeko.Modules.Music.Common;
 using Mewdeko.Modules.Music.CustomPlayer;
@@ -733,6 +734,94 @@ public class SlashMusic(
 
         await player.SetRepeatTypeAsync(repeatType).ConfigureAwait(false);
         await ReplyConfirmLocalizedAsync("music_repeat_type", repeatType).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    ///     Handling track selection for the play command select menu.
+    /// </summary>
+    /// <param name="userId">The original user who summoned the select menu</param>
+    /// <param name="selectedValue">The selected track.</param>
+    [ComponentInteraction("track_select:*", true)]
+    [CheckPermissions]
+    public async Task TrackSelect(ulong userId, string[] selectedValue)
+    {
+        await DeferAsync();
+
+        if (ctx.User.Id != userId) return;
+
+        var componentInteraction = ctx.Interaction as IComponentInteraction;
+
+        var (player, result) = await GetPlayerAsync(false);
+
+        var tracks = await cache.Redis.GetDatabase()
+            .StringGetAsync($"{ctx.User.Id}_{componentInteraction.Message.Id}_tracks");
+
+        var trackList = JsonSerializer.Deserialize<List<LavalinkTrack>>(tracks);
+
+        var queue = await cache.GetMusicQueue(ctx.Guild.Id);
+
+        var selectedTracks = selectedValue.Select(i => trackList[Convert.ToInt32(i.Split("_")[1])]).ToList();
+
+        var startIndex = queue.Count + 1;
+        queue.AddRange(
+            selectedTracks.Select(track => new MewdekoTrack(startIndex++, track, new PartialUser
+            {
+                Id = ctx.User.Id, Username = ctx.User.Username, AvatarUrl = ctx.User.GetAvatarUrl()
+            })));
+
+        if (selectedTracks.Count == 1)
+        {
+            var eb = new EmbedBuilder()
+                .WithAuthor(GetText("music_added"))
+                .WithDescription($"[{selectedTracks[0].Title}]({selectedTracks[0].Uri}) by {selectedTracks[0].Author}")
+                .WithImageUrl(selectedTracks[0].ArtworkUri.ToString())
+                .WithOkColor();
+
+            await FollowupAsync(embed: eb.Build());
+        }
+        else
+        {
+            var paginator = new LazyPaginatorBuilder().AddUser(ctx.User)
+                .WithPageFactory(PageFactory)
+                .WithFooter(PaginatorFooter.PageNumber | PaginatorFooter.Users)
+                .WithMaxPageIndex(queue.Count / 10)
+                .WithDefaultEmotes()
+                .WithActionOnCancellation(ActionOnStop.DeleteMessage)
+                .Build();
+
+            await interactiveService.SendPaginatorAsync(paginator, ctx.Interaction, TimeSpan.FromMinutes(5),
+                InteractionResponseType.DeferredChannelMessageWithSource);
+
+            async Task<PageBuilder> PageFactory(int index)
+            {
+                await Task.CompletedTask;
+                var tracks = queue.Skip(index * 10).Take(10).ToList();
+                var sb = new StringBuilder();
+                foreach (var track in tracks)
+                {
+                    sb.AppendLine($"{track.Index}. [{track.Track.Title}]({track.Track.Uri})");
+                }
+
+                return new PageBuilder()
+                    .WithTitle($"Queue - {queue.Count} tracks")
+                    .WithDescription(sb.ToString())
+                    .WithOkColor();
+            }
+        }
+
+        if (player.CurrentItem == null)
+        {
+            await cache.SetCurrentTrack(ctx.Guild.Id,
+                new MewdekoTrack(1, selectedTracks[0], new PartialUser
+                {
+                    Id = ctx.User.Id, Username = ctx.User.Username, AvatarUrl = ctx.User.GetAvatarUrl()
+                }));
+            await player.PlayAsync(selectedTracks[0]);
+        }
+
+        await cache.SetMusicQueue(ctx.Guild.Id, queue);
+        await cache.Redis.GetDatabase().KeyDeleteAsync($"{ctx.User.Id}_{componentInteraction.Message.Id}_tracks");
+        await ctx.Channel.DeleteMessageAsync(componentInteraction.Message.Id);
     }
 
     private async ValueTask<(MewdekoPlayer, string?)> GetPlayerAsync(bool connectToVoiceChannel = true)
