cd28f79b0a7c2f66ff2acf7faaeeb054e930eb92
SylveonDeko <59923820+SylveonDeko@users.noreply.github.com>
2024-10-11 12:59:14 -0400

    oops

---
+++ src/Mewdeko/Database/Models/DashboardBotUrls.cs
@@ -0,0 +1,12 @@
+﻿namespace Mewdeko.Database.Models;
+
+/// <summary>
+/// Class to get urls for each bot that is able to use a dashboard. Is entirely
+/// </summary>
+public class LocalBotInstances : DbEntity
+{
+    /// <summary>
+    /// The instances url
+    /// </summary>
+    public string BotUrl { get; set; }
+}
